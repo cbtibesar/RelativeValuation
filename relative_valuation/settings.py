639350fc405fda4dd8dca57181460726fa2e9748
Conrad Tibesar <conradtibesar@Conrads-MacBook-Pro.local>
2021-08-27 00:57:08 -0500

    switch up Middleware config

--- relative_valuation/settings.py
+++ relative_valuation/settings.py
@@ -51,10 +51,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
